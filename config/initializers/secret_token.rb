05728d06652bbf50c3d3c7a5a784702198e571cc
mrrussellong <mrrussellong@yahoo.com>
2011-04-04 22:59:11 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Celine::Application.config.secret_token = '8d3645b0e445ca4bbdd9c53df0129b4c33dfad18b5e3e0bda1c686caedf599b50f1604a4bedfd2ba9efed7d8c72c511a3120b8db3057c4c344beb27bfba3f9ef'
